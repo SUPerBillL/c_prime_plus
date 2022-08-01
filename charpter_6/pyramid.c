6f6da30de7bfeb52cf841fc411b2ebdbde32c83b
mingxi.ma <846350668@qq.com>
2022-08-01 11:19:12 +0800

    feat: 新增第6章练习题6

---
+++ charpter_6/pyramid.c
@@ -0,0 +1,44 @@
+/*
+编写一个程序，提示用户输入大写字母，使用嵌套循环以下面金字塔型的格式打印字母
+*/
+#include <stdio.h>
+#define START_ALPHA 'A'
+
+int main(void)
+{
+    char user_input, element;
+    int line, rank, index, line_counter;
+
+    printf("Input start alpha:");
+    scanf("%c", &user_input);
+
+    for (line = 0; line < user_input - START_ALPHA + 1; line++)
+    /* 外层循环处理行转换 */
+    {
+        int i, j, h;
+        element = START_ALPHA - 1;
+        /* 处理留白 */
+        for (i = 0; i < user_input - START_ALPHA - line + 1; i++)
+        {
+            printf(" ");
+        }
+
+        /* 处理增字符 */
+        for (j = 0; j < line + 1; j++)
+        {
+            element++;
+            printf("%c", element);
+        }
+
+        /* 处理减字符 */
+        for (h = 0; h < line; h++)
+        {
+            element--;
+            printf("%c", element);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
