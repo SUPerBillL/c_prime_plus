ab36ac8d6d5a4660440812af220a9d29efedeb2b
mingxi.ma <846350668@qq.com>
2022-08-04 18:27:25 +0800

    feat:第6章练习题代码

--- ch06/ex_18.c
+++ ch06/ex_18.c
@@ -11,3 +11,21 @@
 // exceeds Dunbar’s number. Dunbar’s number is a rough estimate of the maximum
 // size of a cohesive social group in which each member knows every other
 // member and how they relate to one another. Its approximate value is 150.
+#include <stdio.h>
+#define DUNBAR_NUMBER 150
+
+int main(void)
+{
+    int friends = 5;
+    int week;
+
+    for (week = 1; friends < DUNBAR_NUMBER; week++)
+    {
+        friends -= week;
+        friends *= 2;
+        printf("Professor's friends: %d\n", friends);
+    }
+    printf("After %d weeks Professor's friends is over DUNBAR_NUMBER\n", week);
+
+    return 0;
+}
