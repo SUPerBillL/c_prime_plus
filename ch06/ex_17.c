655ca6f773f8c808309b597a27bc8c466c361ddb
mingxi.ma <846350668@qq.com>
2022-08-04 18:17:39 +0800

    feat:第6章练习题代码

--- ch06/ex_17.c
+++ ch06/ex_17.c
@@ -6,3 +6,24 @@
 // withdraws $100,000. Write a program that finds out how many years it takes
 // for Chuckie to empty his account.
 #include <stdio.h>
+#define MONEY 1000000
+#define TAKED_MONEY_PER_YEAR 100000
+#define INTEREST_RATE .08
+
+int main(void)
+{
+    int years = 1;
+    float account_balance = MONEY;
+
+    do
+    {
+        account_balance *= 1.0f + INTEREST_RATE;
+        account_balance -= TAKED_MONEY_PER_YEAR;
+        // printf("account balance: %f\n", account_balance);
+        years++;
+    } while (account_balance > 0);
+
+    printf("After %d years Chuckie take all of the money account_balance!\n", years);
+
+    return 0;
+}
